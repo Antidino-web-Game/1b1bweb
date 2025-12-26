fe074b4845f461d839206ba5fdd9aca9d2e6bd06
Antidino-web-Game <veilleantoine35@gmail.com>
2025-12-26 17:57:52 +0000

    add index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Petit script pour tester l'affichage
+$nom_serveur = "1b1b Anarchy";
+echo "<h1>Bienvenue sur $nom_serveur</h1>";
+echo "<p>Système de duplication : <b>ACTIF</b> (Plugin maison)</p>";
+echo "<p>Classement des Factions : <i>Bientôt disponible</i></p>";
+?>
